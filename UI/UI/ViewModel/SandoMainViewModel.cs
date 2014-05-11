4b4d30366d4e579bc7955196841f90a59f922827
LingForCC <CNCOLIU@CN-L-6790169.asiapacific.abb.com>
2014-05-12 02:56:44 +0800

    Setup the basic framework for MVVMTest. It seems that the basic framework is running.

---
+++ UI/UI/ViewModel/SandoMainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Sando.UI.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sando.UI.ViewModel
+{
+    public class SandoMainViewModel : BaseViewModel
+    {
+
+        #region Properties
+
+        public MenuViewModel MenuViewModel
+        {
+            get;
+            private set;
+        }
+
+        public SearchViewModel SearchViewModel
+        {
+            get;
+            private set;
+        }
+
+        public SearchResultViewModel SearchResultViewModel
+        {
+            get;
+            private set;
+        }
+
+        #endregion
+
+        public SandoMainViewModel()
+        {
+
+            //Since sando is not a heavy UI application, we just load view models here.
+            this.MenuViewModel = new MenuViewModel();
+            this.SearchResultViewModel = new SearchResultViewModel();
+            this.SearchViewModel = new SearchViewModel();
+
+        }
+
+
+    }
+}
---
+++ UI/UI/ViewModel/SearchResultViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Sando.UI.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sando.UI.ViewModel
+{
+    public class SearchResultViewModel : BaseViewModel
+    {
+    }
+}
