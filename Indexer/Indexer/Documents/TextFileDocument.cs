fbc7c0b021b94263cf988ad3f5b10a64c94397bd
Kosta Damevski <kostadin@gmail.com>
2014-06-03 14:34:52 -0400

    Files that didn't commit before

---
+++ Indexer/Indexer/Documents/TextFileDocument.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Lucene.Net.Documents;
+using Sando.ExtensionContracts.ProgramElementContracts;
+using System.Collections.Generic;
+
+namespace Sando.Indexer.Documents
+{
+	public class TextFileDocument : SandoDocument
+	{
+		public TextFileDocument(TextFileElement classElement)
+			: base(classElement)
+		{
+		}
+
+		public TextFileDocument(Document document)
+			: base(document)
+		{
+		}
+
+        public override List<Field> GetFieldsForLucene()
+		{
+            var fields = new List<Field>();
+			var textLineElement = (TextFileElement) programElement;
+            AddBodyField(fields, new Field(SandoField.Body.ToString(), textLineElement.Body, Field.Store.NO, Field.Index.ANALYZED));
+            return fields;
+		}
+
+        public override object[] GetParametersForConstructor(string name, ProgramElementType programElementType, string fullFilePath, int definitionLineNumber, int definitionColumnNumber, string snippet, Document document)
+		{
+            string body = "not stored in index";//document.GetField(SandoField.Body.ToString()).StringValue();
+            return new object[] { name, definitionLineNumber, definitionColumnNumber, fullFilePath, snippet, body };
+		}
+	}
+}
