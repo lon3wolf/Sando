bf66608ee955f8514a065a8b8c6e4b9e9d72d965
Kostadin Damevski <kostadin@gmail.com>
2014-06-13 10:53:45 -0400

    fixed why text files were not appearing as search results

--- UI/UI/ViewModel/SearchViewModel.cs
+++ UI/UI/ViewModel/SearchViewModel.cs
@@ -474,8 +474,8 @@ namespace Sando.UI.ViewModel
                     new ProgramElementWrapper(true, ProgramElementType.MethodPrototype),
                     new ProgramElementWrapper(true, ProgramElementType.Property),
                     new ProgramElementWrapper(true, ProgramElementType.Struct),
-                    new ProgramElementWrapper(true, ProgramElementType.XmlElement)
-                    //new ProgramElementWrapper(true, ProgramElementType.TextLine)
+                    new ProgramElementWrapper(true, ProgramElementType.XmlElement),
+                    new ProgramElementWrapper(true, ProgramElementType.TextFile)
                 };
         }
 
