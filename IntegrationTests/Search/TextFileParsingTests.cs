7cdfcefcaa61b266fd1873c9c215c678d1594e0f
David C. Shepherd <david.shepherd@us.abb.com>
2014-09-15 23:41:42 -0400

    Ensuring that every file that is parsed with a language parser is *also* parsed with the plain text parser.

---
+++ IntegrationTests/Search/TextFileParsingTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using Sando.ExtensionContracts.ProgramElementContracts;
+using Sando.ExtensionContracts.ResultsReordererContracts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestHelpers;
+
+namespace Sando.IntegrationTests.Search
+{
+    public class TextFileParsingTests: AutomaticallyIndexingTestClass
+	{
+        [Test]
+        public void ParseClassFileLikeATestFile()
+        {
+            string keywords = "\"0.7.4.1\"";
+            var expectedLowestRank = 2;
+            Predicate<CodeSearchResult> predicate = el => el.Name.Equals("noclassclass.cs");
+            List<CodeSearchResult> results = EnsureRankingPrettyGood(keywords, predicate, expectedLowestRank);
+        }
+
+		
+        //[Test]
+        public void LargeTextFileSearch()
+        {
+            string keywords = "\"<CommandTable xmlns\"";
+            var expectedLowestRank = 3;
+            Predicate<CodeSearchResult> predicate = el => el.Name.Equals("codemaidtest.txt");
+            List<CodeSearchResult> results = EnsureRankingPrettyGood(keywords, predicate, expectedLowestRank);
+        }
+
+        public override string GetIndexDirName()
+        {
+            return "TextFilesTesting";
+        }
+
+        public override string GetFilesDirectory()
+        {
+            //return "..\\..\\IntegrationTests\\TestFiles";
+            return Path.Combine(TestUtils.SolutionDirectory, "IntegrationTests", "TestFiles", "TextFilesTestFiles");
+        }
+
+        public override TimeSpan? GetTimeToCommit()
+        {
+            return TimeSpan.FromSeconds(4);
+        }
+    
+    }
+}
