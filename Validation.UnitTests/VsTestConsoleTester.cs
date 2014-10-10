86fa8dc58827e660476967c4eed812a1cd5e682d
Kosta Damevski <kostadin@gmail.com>
2014-10-10 16:21:38 -0400

    beginning of implementation to discover tests in solution

---
+++ Validation.UnitTests/VsTestConsoleTester.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+using EnvDTE80;
+using NUnit.Framework;
+using UnitTestHelpers;
+
+namespace Validation.UnitTests
+{
+    [TestFixture]
+    //[Ignore]
+    public class VsTestConsoleTester
+    {
+        private VsTestConsoleRunner _testRunner;
+        private DTE2 dte;
+
+        [TestFixtureSetUp]
+		public void FixtureSetUp() 
+        {            
+            Type type = System.Type.GetTypeFromProgID("VisualStudio.DTE.12.0");
+            object inst = System.Activator.CreateInstance(type, true);
+
+            //takes a while for VS to start up
+            System.Threading.Thread.Sleep(5000);
+
+            dte = (DTE2)inst;
+            var solutionPath = Path.Combine(TestUtils.SolutionDirectory, "Sando.sln");
+            dte.Solution.Open(solutionPath);
+
+            //takes a while to open Sando solution
+            System.Threading.Thread.Sleep(10000);
+
+            _testRunner = new VsTestConsoleRunner();
+		}
+
+        [TestFixtureTearDown]
+        public void FixtureTearDown()
+        {
+            if (dte != null)
+            {
+                dte.Quit();
+            }    
+        }
+
+        [Test]
+        public void TestDiscoverTests()
+        {
+            //_testRunner.DiscoverTests();
+
+        }
+
+        [Test]
+        public void TestFindAllLibrariesInSolution()
+        {
+            _testRunner.FindAllLibrariesInSolution(dte);
+        }
+
+    }
+}
---
+++ Validation/VsTestConsoleRunner.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE80;
+using Sando.DependencyInjection;
+
+namespace Validation
+{
+    public class VsTestConsoleRunner
+    {
+        private string _pathToVsTestConsoleExe;
+        //@"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe";
+
+        public VsTestConsoleRunner()
+        {
+            //determine where the console runner is located and whether it exists
+            _pathToVsTestConsoleExe = "";            
+        }
+
+        /*
+         * returns a pair of test name and the full path of the library containing the test
+         */
+        public List<Tuple<String, String>> DiscoverTests()
+        {
+            var dte = ServiceLocator.Resolve<DTE2>();
+            var libraryList = FindAllLibrariesInSolution(dte);
+            foreach (var library in libraryList)
+            {
+
+                //C:\Users\kosta>"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\
+                //CommonExtensions\Microsoft\TestWindow\vstest.console.exe" /ListTests:"C:\Users\k
+                //osta\Documents\sando\bin\Debug\Parser.UnitTests.dll" /UseVsixExtensions:true
+
+
+            }
+
+            return null;
+        }
+
+        /*
+         * returns the full path of all libraries produced by this solution
+         */ 
+        public List<String> FindAllLibrariesInSolution(DTE2 dte)
+        {
+            var libraryList = new List<String>();
+            var openSolution = dte.Solution;           
+            for (int i = 0; i < openSolution.Projects.Count; i++)
+            {
+                libraryList.Add(openSolution.Projects.Item(i).FullName);
+            }
+            return libraryList;            
+        }
+    }
+}
